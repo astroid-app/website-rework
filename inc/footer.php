3e0004a62a5c078d690138c0670a44dad2b76530
french-femboi <cat@french-femboi.eu>
2024-10-08 17:03:20 +0200

    fixed the 404 page for good, added the datalix link to the footer

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,9 @@
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 justify-center mb-8 lg:grid-cols-2 place-items-center">
                 <img src="/src/assets/img/Astroid-banner.png" alt="Footer Image" class="h-14 w-auto">
-                <img src="https://cdn.datalix.de/images/header.png" alt="Footer Image" class="h-14 w-auto">
+                <a href="https://datalix.eu" target="_blank">
+                    <img src="https://cdn.datalix.de/images/header.png" alt="Footer Image" class="h-14 w-auto">
+                </a>
             </div>
             <div class="max-w-3xl mx-auto">
                     <ul class="text-lg flex items-center justify-center flex-col gap-7 md:flex-row md:gap-12 transition-all duration-500 py-16 mb-10">
