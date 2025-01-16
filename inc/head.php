21eccefdb060316e11dbf1007fb3c2baac26a936
ironswordX <166651652+ironswordX@users.noreply.github.com>
2025-01-16 15:49:48 -0500

    Preload scripts so they load faster

--- inc/head.php
+++ inc/head.php
@@ -1,1 +1,4 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@animxyz/core">
+<!-- Preload scripts so they load faster when they are fetched (loaded) -->
+<link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/7.6.0/polyfill.min.js" as="script" crossorigin="anonymous" />
+<link rel="preload" href="https://cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js?features=Object.assign%2CElement.prototype.append%2CNodeList.prototype.forEach%2CCustomEvent%2Csmoothscroll" as="script" crossorigin="anonymous" />
