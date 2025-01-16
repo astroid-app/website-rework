63d042380d7b213a733f56b2c157d1421e3a955c
ironswordX <166651652+ironswordX@users.noreply.github.com>
2025-01-16 15:55:16 -0500

    Add some preloads on commonly used images

--- inc/head.php
+++ inc/head.php
@@ -2,3 +2,6 @@
 <!-- Preload scripts so they load faster when they are fetched (loaded) -->
 <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/7.6.0/polyfill.min.js" as="script" crossorigin="anonymous" />
 <link rel="preload" href="https://cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js?features=Object.assign%2CElement.prototype.append%2CNodeList.prototype.forEach%2CCustomEvent%2Csmoothscroll" as="script" crossorigin="anonymous" />
+<!-- Preload some commonly used images -->
+<link rel="preload" href="https://astroid.cc/src/assets/img/astroid_logo.png" as="image" />
+<link rel="preload" href="https://astroid.cc/src/assets/img/Astroid-banner.png" as="image" />
