d0bf21691df1b5ec1b7c23f1575087f9f2890714
catpawz <cat@catpawz.net>
2025-05-04 13:10:18 +0200

    Refactor code structure for improved readability and maintainability

--- inc/nav.php
+++ inc/nav.php
@@ -1,7 +1,7 @@
 <nav class="nav">
     <div class="container">
         <a href="/">
-            <img class="logo" src="/src/assets/img/Astroid-banner.png" alt="LOGO" style="height: 35px;margin-left: 20px;">
+            <img class="logo" src="/src/assets/img/astroid_banner_crop.png" alt="LOGO" style="height: 35px;margin-left: 20px;">
         </a>
         <ul class=".nav.active">
             <li><a href="/">Home</a></li>
