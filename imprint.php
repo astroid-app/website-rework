56a089ef6f34c9d4cb2cf1cdf4c52b920566b172
Jason <81298350+Deutscher775@users.noreply.github.com>
2025-07-29 16:17:10 +0200

    Update imprint.php

--- imprint.php
+++ imprint.php
@@ -71,7 +71,7 @@
         Telephone: <a href="tel:+4915901147061" class="text-blue-400 hover:underline">+49 159 01147061</a> (no support)<br>
         E-Mail: <a href="mailto:mail@deutscher775.de" class="text-blue-400 hover:underline">mail@deutscher775.de</a> (no support)
     </p>
-    <h2>Scope</h2>
+    <h2 class="text-2xl font-semibold">Scope</h2>
     <p>This imprint is valid for the following domains:</p>
     <ul>
         <li>*.astroid.cc</li>
