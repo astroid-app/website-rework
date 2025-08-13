6b1afb6babb854a73783e1df30e7ccf5988773a7
Jason <81298350+Deutscher775@users.noreply.github.com>
2025-08-13 16:45:53 +0200

    he changed his github username too TwT

--- about-us.php
+++ about-us.php
@@ -128,7 +128,7 @@
           <h3 class="mt-4 text-lg font-medium text-white dark:text-gray-900">Catpawz</h3>
           <p class="mt-2 text-base text-gray-300 dark:text-gray-500">Frontend & Backend developer</p>
           <p class="mt-2 text-base text-gray-500 dark:text-gray-500">JS, HTML, CSS, PHP, Java</p>
-          <a href="https://github.com/french_femboi" class="mt-4 text-base text-purple-500 hover:text-purple-400">View GitHub profile</a>
+          <a href="https://github.com/catpawzz" class="mt-4 text-base text-purple-500 hover:text-purple-400">View GitHub profile</a>
         </div>
 
         <div class="p-6 bg-gray-800 dark:bg-gray-700 rounded-lg shadow-lg">
